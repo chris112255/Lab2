5fcb99b8ac4c0c9d65269bff3cef173229f6c455
Chris <chris.zheng@mail.utoronto.ca>
2025-05-15 19:08:58 -0400

    commit

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -5,6 +5,7 @@ public class DataTypes {
 
         long s = 0;
         // below is a "foreach" loop which iterates through numbers
+        // done
         for (int x : numbers) {
             s += x;
         }
